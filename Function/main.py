c5b80946df0d0e2874c4733734d328ca72051079
Amoh - Gyebi Godwin <amohgyebigodwin@gmail.com>
2018-02-18 14:35:49 +0000

    Add files via upload

--- Function/main.py
+++ Function/main.py
@@ -1,2 +1,20 @@
 # -*- coding: utf-8 -*-
 # To God be the Glory, great things He hath done
+
+from PyQt5.QtGui import QGuiApplication
+from PyQt5.QtQml import QQmlApplicationEngine
+from PyQt5.QtCore import QObject, pyqtSignal, pyqtSlot; import sys
+
+class Bible(QObject):
+    
+    def __init__(self):
+        
+        QObject.__init__(self)
+        
+app = QGuiApplication(sys.argv)
+bible = Bible()
+engine = QQmlApplicationEngine()
+engine.rootContext().setContextProperty('Bible', bible)
+engine.load('..\\UI\\ui.qml')
+engine.quit.connect(app.quit)
+sys.exit(app.exec_())
